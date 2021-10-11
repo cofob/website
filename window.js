7c0c0e7dda85674d13a864e2da4417a3797f5ae3
VALODA-27 <igornek2016@gmail.com>
2021-10-11 19:06:15 +0300

    minor correction of layout errors and new design
    Changed the background, completely redesigned the list of projects, fixed layout errors, deleted or moved some files

--- window.js
+++ window.js
@@ -1,1 +1,37 @@
-function openwindow(){document.getElementById("menu_par").style.margin="0 0 0 0px",document.getElementById("close_window").style.left="0",document.getElementById("boddy").style.position="fixed"}function closewindow(){document.getElementById("menu_par").style.margin="0 0 0 -100%",document.getElementById("close_window").style.left="-100px",document.getElementById("boddy").style.position="inherit"}
+
+let windowminecraft = document.getElementById("window_main-minecraft");// название переменых в нижнем регистре
+let windowyggdrasil = document.getElementById("window_main-yggdrasil");
+let windowdns = document.getElementById("window_main-dns");
+
+
+function widnowMinecraftserver() { // имя функции windows+название пректа с заглавной буквы без пробела на латинице
+	windowminecraft.classList.toggle("show");//вызов анимации появления
+	windowminecraft.style.visibility = "visible";//изменение стиля невидимого окна на видимое
+}
+window.addEventListener("click", function(event) { //закрытие окна при нажатии на затемненую область
+    if (event.target == windowminecraft) {
+        windowminecraft.style.visibility = "hidden";//изменение стиля видимого окна на невидимое
+    }
+});
+
+
+function windowYggdrasil() {
+	windowyggdrasil.classList.toggle("show");
+	windowyggdrasil.style.visibility = "visible";
+}
+window.addEventListener("click", function(event) {
+    if (event.target == windowyggdrasil) {
+        windowyggdrasil.style.visibility = "hidden";
+    }
+});
+
+
+function windowDns() {
+	windowdns.classList.toggle("show");
+	windowdns.style.visibility = "visible";
+}
+window.addEventListener("click", function(event) {
+    if (event.target == windowdns) {
+        windowdns.style.visibility = "hidden";
+    }
+});
