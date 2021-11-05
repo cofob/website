bf1c94fcdbb6c878c5af45bd124660e033791468
VALODA-27 <igornek2016@gmail.com>
2021-11-05 17:39:59 +0300

    ᅠ

--- script.js
+++ script.js
@@ -46,17 +46,27 @@ window.addEventListener("click", function(event) {
 
 
 const titlePrlx = document.getElementById("title");
+const ParentTitlePrlx = document.getElementById("par_h");
 
 document.addEventListener("mousemove", function (e) { MoveTitle(e); });
 
 function MoveTitle(e)
 {
-	let offsetX = (e.clientX / window.innerWidth * -40) + 20;
-	let offsetY = (e.clientY / window.innerWidth * -20) + 10;
+	let offsetX = (e.clientX / window.innerWidth * -60) + 30;
+	let offsetY = (e.clientY / window.innerWidth * -30) + 15;
 	let i = (e.clientY / window.innerHeight * 8) - 4;
 	let offsetSmnsX = (e.clientX / window.innerWidth * -10) + 5;
 	let offsetSX = (e.clientX / window.innerWidth * 10) - 5;
-	let offsetSmnsY = (e.clientY / window.innerHeight * -8) + 4;
+	let offsetSmnsY = (e.clientY / window.innerHeight * - 10) + 5;
 	let offsetSY = (e.clientY / window.innerHeight * 8) - 4;
    titlePrlx.setAttribute("style", "transform: translateX(" + offsetX + "px)" + " translateY(" +  offsetY + "px) rotateY(" + offsetSmnsX + "deg) rotateX(" + i + "deg);" + "text-shadow: " + offsetSmnsX + "px " + offsetSmnsY + "px 0px #A72F31," + offsetSX + "px " + offsetSY + "px 0px #5A48D0;" );
 }
+
+window.addEventListener("scroll", PrlxTitle);
+
+function PrlxTitle(){
+	let prlx = (window.pageYOffset / 3);
+	ParentTitlePrlx.setAttribute("style", "transform: translateY(" + prlx + "px");
+}
+
+
